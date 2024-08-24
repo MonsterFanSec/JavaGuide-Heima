d509ce8bb81508f9d098f2416aace405e69fdbdb
daykalif <1038200595@qq.com>
2024-08-24 16:12:30 +0800

    feat: p89 面向对象-09-this的内存原理

---
+++ JavaBase1/src/day08/src/com/itheima/test7/Test2.java
@@ -0,0 +1,15 @@
+package com.itheima.test7;
+
+public class Test2 {
+	/**
+	 * this的作用：区分局部变量和成员变量
+	 * this的本质：所在方法调用者的地址值（也就是方法调用者的地址值）
+	 *
+	 * 如：Student s = new Student();
+	 * 	  s.method();
+	 *
+	 * 方法：method
+	 * 调用者：s， s的地址值：001
+	 * this：001
+	 */
+}
