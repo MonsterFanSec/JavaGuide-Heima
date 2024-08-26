6016df74224c0c8950cfb64f1d518fbabca5280c
daykalif <1038200595@qq.com>
2024-08-26 22:39:55 +0800

    feat: p91-面向对象综合训练-01-文字版格斗游戏

---
+++ JavaBase1/src/day09-面向对象综合练习/src/com/itheima/test2/Test.java
@@ -0,0 +1,9 @@
+package com.itheima.test2;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.printf("%s", "张三");
+		System.out.println();
+		System.out.printf("%s你好%s", "李四", "王五");
+	}
+}
