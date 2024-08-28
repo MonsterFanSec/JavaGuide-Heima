df071ddf6f7af29872bcd8ee0f623c072f0bbf1f
daykalif <1038200595@qq.com>
2024-08-28 23:01:46 +0800

    feat: p97-字符串-02-String概述

---
+++ JavaBase1/src/day10-字符串/src/com/itheima/test1/Test.java
@@ -0,0 +1,25 @@
+package com.itheima.test1;
+
+import java.util.Random;
+
+public class Test {
+	/*
+	 * API:
+	 * 		API(Application Programming Interface):应用程序编程接口
+	 *
+	 * 简单理解：
+	 * 		API就是别人已经写好的东西，我们不需要自己编写，直接使用即可。
+	 *
+	 * 			public static void main(String[] args) {
+	 *				Random r = new Random();
+	 *				int number = r.nextInt();
+	 *			}
+	 *
+	 * API和API帮助文档
+	 * 		Java API：
+	 * 				目前是JDK中提供的各种功能的Java类。
+	 * 				这些类将底层的实现封装了起来，我们不需要关心这些类是如何实现的，只需要学习这些类如何使用即可。
+	 * 		API帮助文档：
+	 * 				帮助开发人员更好的使用API和查询API的一个工具。
+	 */
+}
---
+++ JavaBase1/src/day10-字符串/src/com/itheima/test1/Test2.java
@@ -0,0 +1,29 @@
+package com.itheima.test1;
+
+public class Test2 {
+	/*
+	 *	String概述：
+	 * 		java.lang.String类代表字符串，Java程序中的所有字符串文字（如："abc"）都为此类的对象。
+	 *
+	 * 		String name = "尼古拉斯·阿维";
+	 * 		String schoolName = "黑马程序员";
+	 *
+	 * 	String的注意点：
+	 * 		字符串的内容是不会发生改变的，它的对象在创建后不能被更改。
+	 *
+	 *
+	 *	创建了3个对象：
+	 * 		String name = "尼古拉斯·阿维";
+	 * 		String schoolName = "黑马程序员";
+	 *		System.out.println(name + schoolName);	(字符串拼接产生一个新的字符串)
+	 *
+	 *  创建了2个对象：
+	 * 		String name = "尼古拉斯·阿维";
+	 * 		name = "三连加投币·阿维";
+	 *
+	 * 	总结：
+	 * 		1.String是Java定义好的一个类。定义在java.lang包中，所以使用的时候不需要导包。
+	 * 		2.Java程序中的所有字符串文字（例如："abc"）都被视为此类的对象。
+	 * 		3.字符串不可变，它们的值在创建后不能被更改。
+	 */
+}
