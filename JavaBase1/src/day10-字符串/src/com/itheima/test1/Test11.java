7627ad906234f8b2f74776d33f9a89bc06048e68
daykalif <1038200595@qq.com>
2024-09-01 13:26:28 +0800

    feat: p104-字符串-09-练习-游戏骂人敏感词替换

---
+++ JavaBase1/src/day10-字符串/src/com/itheima/test1/Test11.java
@@ -0,0 +1,14 @@
+package com.itheima.test1;
+
+public class Test11 {
+	public static void main(String[] args) {
+		//1.获取到说的话
+		String talk = "你玩的真好，以后不要再玩了，TMD";
+
+		//2.把里面的敏感词TMD替换为***
+		String result = talk.replace("TMD", "***");
+
+		//3.打印结果
+		System.out.println(result);
+	}
+}
