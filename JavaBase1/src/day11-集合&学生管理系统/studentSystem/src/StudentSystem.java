fd936f39ac8de2b109f6d00b3a54c16f51ca9cc2
daykalif <1038200595@qq.com>
2024-09-04 16:49:19 +0800

    feat: p118-学生管理系统-03-删除和修改

--- JavaBase1/src/day11-集合&学生管理系统/studentSystem/src/StudentSystem.java
+++ JavaBase1/src/day11-集合&学生管理系统/studentSystem/src/StudentSystem.java
@@ -78,6 +78,76 @@ public class StudentSystem {
 		System.out.println("学生信息添加成功");
 	}
 
+	// 删除学生
+	public static void deleteStudent(ArrayList<Student> list) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("请输入要删除的id");
+		String id = sc.next();
+
+		// 查询id在集合中的索引
+		int index = getIndex(list, id);
+		// 对index进行判断，如果-1，就表示不存在，结束方法，回到初始菜单
+		if (index >= 0) {
+			// 如果大于等于0的，表示存在，直接删除
+			list.remove(index);
+			System.out.println("id为：" + id + "的学生删除成功");
+		} else {
+			System.out.println("id不存在，删除失败");
+		}
+	}
+
+	// 修改学生
+	public static void updateStudent(ArrayList<Student> list) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("请输入要修改学生的id");
+		String id = sc.next();
+
+		int index = getIndex(list, id);
+
+		if (index == -1) {
+			System.out.println("要修改的id" + id + "不存在，请重新输入");
+			return;
+		}
+
+		// 当代码执行到这里，表示什么？表示当前id是存在的。
+		// 根据id获取要修改的学生对象
+		Student stu = list.get(index);
+
+		//输入其他的信息并修改
+		System.out.println("请输入要修改的学生姓名");
+		String newName = sc.next();
+		stu.setName(newName);
+
+		System.out.println("请输入要修改的学生年龄");
+		int newAge = sc.nextInt();
+		stu.setAge(newAge);
+
+		System.out.println("请输入要修改的学生家庭住址");
+		String newAddress = sc.next();
+		stu.setAddress(newAddress);
+
+		System.out.println("学生信息修改成功");
+	}
+
+	// 查询学生
+	public static void queryStudent(ArrayList<Student> list) {
+		if (list.size() == 0) {
+			System.out.println("当前无学生信息，请添加后再查询");
+			//结束方法
+			return;
+		}
+
+		// 打印表头信息
+		System.out.println("id\t\t\t姓名\t年龄\t家庭住址");    // \t：制表符，长度可变的空格，需要补齐8个字符，或者8的倍数
+
+		// 当代码执行到这里，表示集合中是有数据的
+		for (int i = 0; i < list.size(); i++) {
+			Student stu = list.get(i);
+			System.out.println(stu.getId() + "\t" + stu.getName() + "\t" + stu.getAge() + "\t" + stu.getAddress());
+		}
+	}
+
+
 	// 判断id在集合中是否存在
 	public static boolean contains(ArrayList<Student> list, String id) {
 		// 循环遍历集合得到里面的每一个学生对象
@@ -113,34 +183,5 @@ public class StudentSystem {
 		}
 		// 当循环结束之后还没有找到，就表示不存在，返回-1.
 		return -1;
-	}
-
-
-	// 删除学生
-	public static void deleteStudent(ArrayList<Student> list) {
-		System.out.println("deleteStudent");
-	}
-
-	// 修改学生
-	public static void updateStudent(ArrayList<Student> list) {
-		System.out.println("updateStudent");
-	}
-
-	// 查询学生
-	public static void queryStudent(ArrayList<Student> list) {
-		if (list.size() == 0) {
-			System.out.println("当前无学生信息，请添加后再查询");
-			//结束方法
-			return;
-		}
-
-		// 打印表头信息
-		System.out.println("id\t\t\t姓名\t年龄\t家庭住址");    // \t：制表符，长度可变的空格，需要补齐8个字符，或者8的倍数
-
-		// 当代码执行到这里，表示集合中是有数据的
-		for (int i = 0; i < list.size(); i++) {
-			Student stu = list.get(i);
-			System.out.println(stu.getId() + "\t" + stu.getName() + "\t" + stu.getAge() + "\t" + stu.getAddress());
-		}
 	}
 }
