c9af9f7e73d46e705ec5bf8341a7ed61925fb8ce
daykalif <1038200595@qq.com>
2024-09-05 12:51:13 +0800

    feat: p120-学生管理系统升级-02-完整代码实现-登录功能实现

--- JavaBase1/src/day12-学生管理系统升级版/studentSystemUpdate/src/com/itheima/system/App.java
+++ JavaBase1/src/day12-学生管理系统升级版/studentSystemUpdate/src/com/itheima/system/App.java
@@ -1,6 +1,7 @@
 package com.itheima.system;
 
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class App {
@@ -26,12 +27,9 @@ public class App {
 		}
 	}
 
-
-	private static void login(ArrayList<User> list) {
-		System.out.println("登录");
-	}
-
-
+	/**
+	 * 1.注册
+	 */
 	private static void register(ArrayList<User> list) {
 		/* 1.键盘录入用户名 */
 		Scanner sc = new Scanner(System.in);
@@ -124,12 +122,6 @@ public class App {
 		printList(list);
 	}
 
-
-	private static void forgetPassword(ArrayList<User> list) {
-		System.out.println("忘记密码");
-	}
-
-
 	/**
 	 * 注册时判断用户名是否满足条件：
 	 * 1.用户名长度必须在3~15位之间
@@ -174,7 +166,6 @@ public class App {
 		return count > 0;
 	}
 
-
 	/**
 	 * 循环遍历集合得到每一个用户对象，并拿着用户对象中的用户名进行比较
 	 */
@@ -190,7 +181,6 @@ public class App {
 		// 当循环结束了，表示集合里面所有的用户都比较完毕了，还没有一样的，则返回false
 		return false;
 	}
-
 
 	/**
 	 * 校验：身份证号码
@@ -229,7 +219,6 @@ public class App {
 		}
 	}
 
-
 	/**
 	 * 校验手机号：
 	 * 1.长度为11位
@@ -257,7 +246,6 @@ public class App {
 		return true;
 	}
 
-
 	/**
 	 * 遍历输出user list
 	 */
@@ -267,5 +255,123 @@ public class App {
 			User user = list.get(i);
 			System.out.println(user.getUsername() + ", " + user.getPassword() + ", " + user.getPersonID() + ", " + user.getPhoneNumber());
 		}
+	}
+
+
+	/**
+	 * 2.登录
+	 */
+	private static void login(ArrayList<User> list) {
+		Scanner sc = new Scanner(System.in);
+		for (int i = 0; i < 3; i++) {
+			System.out.println("请输入用户名");
+			String username = sc.next();
+
+			//判断用户名是否存在
+			boolean flag = contains(list, username);
+			if (!flag) {
+				System.out.println("用户名" + username + "未注册，请先注册再登录");
+				return;
+			}
+
+			System.out.println("请输入密码");
+			String password = sc.next();    // 快捷键：command + v
+
+			while (true) {
+				String rightCode = getCode();    // 每次都会生成不同的验证码
+				System.out.println("当前正确的验证码为：" + rightCode);
+
+				System.out.println("请输入验证码");
+				String code = sc.next();
+				if (!code.equalsIgnoreCase(rightCode)) {
+					System.out.println("验证码错误");
+					continue;
+				} else {
+					System.out.println("验证码正确");
+					break;
+				}
+			}
+
+			/*
+			 * 验证用户名和密码是否正确：（即集合中是否包含用户名和密码）
+			 * 		定义一个方法验证用户名和密码是否正确
+			 *		- 封装思想的应用：
+			 * 			我们可以把一些零散的数据，封装成一个对象
+			 * 			以后传递参数的时候，只要传递一个整体就可以了，不需要管这些零散的数据。
+			 */
+			User useInfo = new User(username, password, null, null);
+			boolean result = checkUserInfo(list, useInfo);    // 快速创建方法，快捷键：option + Enter
+			if (result) {
+				System.out.println("登录成功，可以开始使用学生管理系统了");
+				break;
+			} else {
+				System.out.println("登录失败，用户名或密码错误");
+				if (i == 2) {
+					System.out.println("当前账号" + username + "被锁定，请联系黑马程序员客服：XXX-XXXXX");
+					//当前账号锁定之后，直接结束方法即可
+					return;
+				} else {
+					System.out.println("用户名或密码错误，还剩下" + (2 - i) + "次机会");
+				}
+			}
+		}
+	}
+
+	/**
+	 * 生成验证码
+	 */
+	private static String getCode() {
+		// 1.创建一个集合添加所有的大写和小写字母
+		ArrayList<Character> list = new ArrayList<>();
+		for (int i = 0; i < 26; i++) {
+			list.add((char) ('a' + i));
+			list.add((char) ('A' + i));
+		}
+
+		StringBuilder sb = new StringBuilder();
+
+		// 2.要随机抽取4个字符
+		Random r = new Random();
+		for (int i = 0; i < 4; i++) {
+			// 获取随机索引
+			int index = r.nextInt(list.size());
+			// 利用随机索引获取字符，JDK5之后，Character和char可以理解为是一样的
+			char c = list.get(index);
+			// 把随机字符添加到sb当中
+			sb.append(c);
+		}
+
+		// 3.把一个随机数字0～9添加到末尾
+		int number = r.nextInt(10);
+		sb.append(number);
+
+		// 4.如果我们要修改字符串中的内容，需要先把字符串变成字符数组，在数组中修改，然后再创建一个新的字符串
+		char[] arr = sb.toString().toCharArray();
+		// 拿着最后一个索引，跟随机索引进行交换
+		int randomIndex = r.nextInt(arr.length);
+		// 最大索引指向的元素 跟随机索引指向的元素交换
+		char temp = arr[randomIndex];
+		arr[randomIndex] = arr[arr.length - 1];
+		arr[arr.length - 1] = temp;
+		return new String(arr);
+	}
+
+	private static boolean checkUserInfo(ArrayList<User> list, User useInfo) {
+		// 遍历集合，判断用户是否存在，如果存在登录成功，如果不存在登录失败
+		for (int i = 0; i < list.size(); i++) {
+			User user = list.get(i);
+			if (user.getUsername().equals(useInfo.getUsername()) && user.getPassword().equals(useInfo.getPassword())) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+
+	/**
+	 * 3.忘记密码
+	 */
+	private static void forgetPassword(ArrayList<User> list) {
+		System.out.println("忘记密码");
 	}
 }
