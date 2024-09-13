e2aa31c93d1002f19be5097274e3cee14b551303
daykalif <1038200595@qq.com>
2024-09-13 10:19:42 +0800

    feat: P128-面向对象进阶-08-调用本类其他构造方法

---
+++ JavaBase1/src/day13-面向对象进阶（static&继承）/src/oopExtends/demo10/Student.java
@@ -0,0 +1,19 @@
+package oopExtends.demo10;
+
+public class Student {
+	String name;
+	int age;
+	String school;
+
+	// 需求：默认为传智大学
+	public Student() {
+		// 表示调用本类其他构造方法，虚拟机就不会再添加super();
+		this(null, 0, "传智大学");
+	}
+
+	public Student(String name, int age, String school) {
+		this.name = name;
+		this.age = age;
+		this.school = school;
+	}
+}
---
+++ JavaBase1/src/day13-面向对象进阶（static&继承）/src/oopExtends/demo10/Test.java
@@ -0,0 +1,12 @@
+package oopExtends.demo10;
+
+/*
+ *	this、super使用总结
+ * 	- this：理解为一个变量，表示当前方法调用者的地址值；
+ * 	- super：代表父类存储空间。
+ */
+public class Test {
+	public static void main(String[] args) {
+		Student s = new Student();
+	}
+}
