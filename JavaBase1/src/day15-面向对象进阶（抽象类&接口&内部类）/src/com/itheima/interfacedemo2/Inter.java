726ca5996895ae16eb39cf9cbd6f75b12b728874
daykalif <1038200595@qq.com>
2024-09-16 13:19:46 +0800

    feat: P137-面向对象进阶-17-接口的细节：成员特点

---
+++ JavaBase1/src/day15-面向对象进阶（抽象类&接口&内部类）/src/com/itheima/interfacedemo2/Inter.java
@@ -0,0 +1,9 @@
+package com.itheima.interfacedemo2;
+
+public interface Inter {
+	// 接口中的变量默认用public static final修饰
+	int a = 10;	// 即：public static final int a = 10；
+
+	// 接口中的方法默认用public abstract修饰
+	void method();
+}
---
+++ JavaBase1/src/day15-面向对象进阶（抽象类&接口&内部类）/src/com/itheima/interfacedemo2/Test.java
@@ -0,0 +1,31 @@
+package com.itheima.interfacedemo2;
+
+import java.util.Scanner;
+
+/*
+ * 接口中成员的特点：
+ * 	- 成员变量：
+ * 		只能是常量
+ * 		默认修饰符：public static final
+ * 	- 构造方法：
+ * 		在接口中没有构造方法
+ * 	- 成员方法：
+ * 		只能是抽象方法
+ * 		默认修饰符：public static
+ *
+ * JDK7以前：接口中只能定义抽象方法。
+ * JDK8的新特性：接口中可以定义有方法体的方法。
+ * JDK9的新特性：接口中可以定义私有方法。
+ */
+public class Test {
+	public static void main(String[] args) {
+		// 创建实现类对象并调用方法
+		InterImpl ii = new InterImpl();
+		ii.method();
+
+
+		// 内存分析工具需要程序一直运行，因此代码中用了一些Scanner
+		Scanner sc = new Scanner(System.in);
+		sc.next();
+	}
+}
