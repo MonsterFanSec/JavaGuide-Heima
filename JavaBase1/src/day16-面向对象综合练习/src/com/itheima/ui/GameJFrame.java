bc6c9bed42bb9fe4517f5743d428c362997de3f2
daykalif <1038200595@qq.com>
2024-09-22 18:00:56 +0800

    feat: P144阶段项目-04-打乱图片

--- JavaBase1/src/day16-面向对象综合练习/src/com/itheima/ui/GameJFrame.java
+++ JavaBase1/src/day16-面向对象综合练习/src/com/itheima/ui/GameJFrame.java
@@ -1,11 +1,16 @@
 package com.itheima.ui;
 
 import javax.swing.*;
+import java.util.Random;
 
 public class GameJFrame extends JFrame {
 	//	在JAVA中，JFrame表示界面/窗体
 	//	与此同时，子类也表示界面/窗体
 	//	规定：GameJFrame这个界面表示的就是游戏的主界面，以后跟游戏相关的所有逻辑都写在这个类中
+
+	// 创建一个二维数组，目的：用来管理数据；加载图片的时候，会根据二维数组中的数据进行加载
+	int[][] data = new int[4][4];
+
 
 	// 空参构造
 	public GameJFrame() {
@@ -15,32 +20,14 @@ public class GameJFrame extends JFrame {
 		// 初始化菜单
 		initJMenuBar();
 
-		// 初始化图片
+		// 初始化数据(打乱图片索引)
+		initData();
+
+		// 初始化图片（根据打乱之后的结果去加载图片）
 		initImage();
 
 		// 让显示显示出来，建议写在最后
 		this.setVisible(true);
-	}
-
-	private void initImage() {
-		int number = 1;
-		//	外循环--把内循环重复执行了4次
-		for (int i = 0; i < 4; i++) {
-			//	内循环--表示在一行添加4张图片
-			for (int j = 0; j < 4; j++) {
-
-				// 创建一个图片ImageIcon的对象，传入路径
-				ImageIcon icon = new ImageIcon("/Users/daykalif/Desktop/Java/JavaBase1/src/day16-面向对象综合练习/image/animal/animal3/" + number + ".jpg");
-				// 创建一个JLabel的对象（管理容器）
-				JLabel jLabel = new JLabel(icon);
-				// 指定图片位置，如果不设置位置,默认会放到界面正中央
-				jLabel.setBounds(105 * j, 105 * i, 105, 105);
-				// 把管理容器添加到界面中
-				this.getContentPane().add(jLabel);
-				// 添加一次之后，number需要自增，表示下一次加载后面一张图片
-				number++;
-			}
-		}
 	}
 
 	private void initJMenuBar() {
@@ -90,5 +77,51 @@ public class GameJFrame extends JFrame {
 
 		// 取消默认的居中放置，只有取消了才会按照XY轴的形式添加组件
 		this.setLayout(null);
+	}
+
+	private void initData() {
+		// 1.定义一个一维数组
+		int[] tempArr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+
+		// 2.打乱数组中的数据的顺序
+		// 遍历数组，得到每一个元素，拿着每一个元素跟随机索引上的数据进行交换
+		Random r = new Random();
+		for (int i = 0; i < tempArr.length; i++) {
+			// 获取到随机索引
+			int index = r.nextInt(tempArr.length);
+
+			// 拿着遍历到的每一个数据，跟随机索引上的数据进行交换
+			int temp = tempArr[i];
+			tempArr[i] = tempArr[index];
+			tempArr[index] = temp;
+		}
+
+		/*
+		 * 3.给二维数组添加数据
+		 */
+		// 解法一：遍历一维数组tempArr得到每一个元素，把每一个元素依次添加到二维数组当中
+		for (int i = 0; i < tempArr.length; i++) {
+			data[i / 4][i % 4] = tempArr[i];
+		}
+	}
+
+	private void initImage() {
+		//	外循环--把内循环重复执行了4次
+		for (int i = 0; i < 4; i++) {
+			//	内循环--表示在一行添加4张图片
+			for (int j = 0; j < 4; j++) {
+				// 获取当前要加载图片的序号
+				int num = data[i][j];
+
+				// 创建一个图片ImageIcon的对象，传入路径
+				ImageIcon icon = new ImageIcon("/Users/daykalif/Desktop/Java/JavaBase1/src/day16-面向对象综合练习/image/animal/animal3/" + num + ".jpg");
+				// 创建一个JLabel的对象（管理容器）
+				JLabel jLabel = new JLabel(icon);
+				// 指定图片位置，如果不设置位置,默认会放到界面正中央
+				jLabel.setBounds(105 * j, 105 * i, 105, 105);
+				// 把管理容器添加到界面中
+				this.getContentPane().add(jLabel);
+			}
+		}
 	}
 }
