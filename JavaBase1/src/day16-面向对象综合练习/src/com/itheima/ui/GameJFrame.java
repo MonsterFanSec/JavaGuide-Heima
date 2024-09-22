1f365ddd224817321ce0552a9ca7273f4af2241c
daykalif <1038200595@qq.com>
2024-09-22 14:35:05 +0800

    feat: P144阶段项目-02-界面设置和菜单搭建

--- JavaBase1/src/day16-面向对象综合练习/src/com/itheima/ui/GameJFrame.java
+++ JavaBase1/src/day16-面向对象综合练习/src/com/itheima/ui/GameJFrame.java
@@ -7,7 +7,48 @@ public class GameJFrame extends JFrame {
 	//	与此同时，子类也表示界面/窗体
 	//	规定：GameJFrame这个界面表示的就是游戏的主界面，以后跟游戏相关的所有逻辑都写在这个类中
 
+	// 空参构造
 	public GameJFrame() {
+		// 初始化界面
+		initJFrame();    // 快捷键：command+option+M（抽取方法）
+
+		// 初始化菜单
+		initJMenuBar();
+
+		//	让显示显示出来，建议写在最后
+		this.setVisible(true);
+	}
+
+	private void initJMenuBar() {
+		// 创建整个菜单对象
+		JMenuBar jMenuBar = new JMenuBar();
+		// 创建菜单上面的两个选项的对象（功能、关于我们）
+		JMenu functionJMenu = new JMenu("功能");
+		JMenu aboutJMenu = new JMenu("关于我们");
+
+		// 创建选项下面的条目对象
+		JMenuItem replayItem = new JMenuItem("重新游戏");
+		JMenuItem reLoginItem = new JMenuItem("重新登录");
+		JMenuItem closeItem = new JMenuItem("关闭游戏");
+
+		JMenuItem accountItem = new JMenuItem("公众号");
+
+		// 将每一个选项下面的条目添加到选项中
+		functionJMenu.add(replayItem);
+		functionJMenu.add(reLoginItem);
+		functionJMenu.add(closeItem);
+
+		aboutJMenu.add(accountItem);
+
+		// 将菜单里面的两个选项添加到菜单当中
+		jMenuBar.add(functionJMenu);
+		jMenuBar.add(aboutJMenu);
+
+		// 给整个界面设置菜单
+		this.setJMenuBar(jMenuBar);
+	}
+
+	private void initJFrame() {
 		//	比如，宽高，直接展示出来
 		this.setSize(630, 680);
 
@@ -22,8 +63,5 @@ public class GameJFrame extends JFrame {
 
 		// 设置关闭模式
 		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-		//	让显示显示出来，建议写在最后
-		this.setVisible(true);
 	}
 }
