15c87683dc5ad7295824e575e9765c6473349409
daykalif <1038200595@qq.com>
2024-09-22 14:17:57 +0800

    feat: P144阶段项目-02-界面设置

--- JavaBase1/src/day16-面向对象综合练习/src/com/itheima/ui/GameJFrame.java
+++ JavaBase1/src/day16-面向对象综合练习/src/com/itheima/ui/GameJFrame.java
@@ -8,7 +8,22 @@ public class GameJFrame extends JFrame {
 	//	规定：GameJFrame这个界面表示的就是游戏的主界面，以后跟游戏相关的所有逻辑都写在这个类中
 
 	public GameJFrame() {
+		//	比如，宽高，直接展示出来
 		this.setSize(630, 680);
+
+		// 设置界面的标题
+		this.setTitle("拼图单机版 v1.0");
+
+		// 设置界面置顶
+		this.setAlwaysOnTop(true);
+
+		// 设置界面居中
+		this.setLocationRelativeTo(null);
+
+		// 设置关闭模式
+		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+		//	让显示显示出来，建议写在最后
 		this.setVisible(true);
 	}
 }
