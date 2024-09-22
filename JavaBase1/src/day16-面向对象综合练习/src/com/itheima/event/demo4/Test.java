771541877235812e90a1ba3cea0908978cfa9de2
daykalif <1038200595@qq.com>
2024-09-22 18:40:00 +0800

    feat: P144阶段项目-05-事件

---
+++ JavaBase1/src/day16-面向对象综合练习/src/com/itheima/event/demo4/Test.java
@@ -0,0 +1,21 @@
+package com.itheima.event.demo4;
+
+/*
+ * 鼠标监听机制-MouseListener
+ * 		- 划入动作
+ * 		- 单击动作
+ * 			- 按下动作
+ * 			- 松开动作
+ * 		- 划出动作
+ *
+ * 	思考：
+ *		1.如果以后我想监听一个按钮的单击事件，有几种方式？
+ * 			- 动作监听
+ * 			- 鼠标监听中的单击事件
+ * 			- 鼠标监听中的松开事件
+ */
+public class Test {
+	public static void main(String[] args) {
+		new MyJFrame();
+	}
+}
---
+++ JavaBase1/src/day16-面向对象综合练习/src/com/itheima/event/demo5/Test.java
@@ -0,0 +1,10 @@
+package com.itheima.event.demo5;
+
+/*
+ *	 键盘监听机制-KeyListener
+ */
+public class Test {
+	public static void main(String[] args) {
+		new MyJFrame();
+	}
+}
