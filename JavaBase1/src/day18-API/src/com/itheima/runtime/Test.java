37e0e8a4de6ce0028d0b6e7b11ac048ce4e05cec
daykalif <1038200595@qq.com>
2024-09-26 07:30:43 +0800

    feat: P158-常用API-04-Runtime

---
+++ JavaBase1/src/day18-API/src/com/itheima/runtime/Test.java
@@ -0,0 +1,11 @@
+package com.itheima.runtime;
+
+/*
+ * Runtime：
+ * 		 Runtime表示当前虚拟机的运行环境
+ */
+public class Test {
+    public static void main(String[] args) {
+        new MyJframe();
+    }
+}
