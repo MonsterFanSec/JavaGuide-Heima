badbe15ea1a524bf40365bd68100fb63ce5a89c8
daykalif <1038200595@qq.com>
2024-09-29 15:13:35 +0800

    feat: 170-常用API-16-JDK7时间-SimpleDateFormat练习

---
+++ JavaBase1/src/day20-API（时间，包装类，练习）/src/com/itheima/date/A05_SimpleDateFormatDemo3.java
@@ -0,0 +1,49 @@
+package com.itheima.date;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/*
+ * 需求:
+ *  秒杀活动开始时间:2023年11月11日 0:0:0(毫秒值)
+ *  秒杀活动结束时间:2023年11月11日 0:10:0(毫秒值)
+ *
+ *  小贾下单并付款的时间为:2023年11月11日 0:01:00
+ *  小皮下单并付款的时间为:2023年11月11日 0:11:00
+ *  用代码说明这两位同学有没有参加上秒杀活动?
+ *
+ *
+ * 总结：
+ * 	1.SimpleDateFormat的两个作用
+ * 		格式化
+ * 		解析
+ *	2.如何指定格式：
+ * 		yyyy年MM月dd日 HH:mm:ss
+ */
+public class A05_SimpleDateFormatDemo3 {
+	public static void main(String[] args) throws ParseException {
+		// 1.定义字符串表示三个时间
+		String startStr = "2023年11月11日 0:0:0";
+		String endStr = "2023年11月11日 0:10:0";
+		String orderStr = "2023年11月11日 0:01:00";
+
+		// 2.解析上面的三个时间，得到Date对象
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日HH:mm:ss");
+		Date startDate = sdf.parse(startStr);
+		Date endDate = sdf.parse(endStr);
+		Date orderDate = sdf.parse(orderStr);
+
+		// 3.得到三个时间的毫秒值
+		long startTime = startDate.getTime();
+		long endTime = endDate.getTime();
+		long orderTime = orderDate.getTime();
+
+		// 4.判断
+		if (orderTime >= startTime && orderTime <= endTime) {
+			System.out.println("参加秒杀活动成功");
+		} else {
+			System.out.println("参加秒杀活动失败");
+		}
+	}
+}
