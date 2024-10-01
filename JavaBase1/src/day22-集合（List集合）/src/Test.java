b10e2a6e9bd69fb1a96e81a91a9d996534a5c882
daykalif <1038200595@qq.com>
2024-10-01 15:50:00 +0800

    p185 集合进阶-01-单列集合顶层接口Collection

---
+++ JavaBase1/src/day22-集合（List集合）/src/Test.java
@@ -0,0 +1,46 @@
+/*
+ * 集合体系结构：(大写字母表示接口，小写字母表示实现类)
+ * 		A、Collection
+ * 			单列集合（添加数据的时候，每次只能单个数据）
+ * 			AA.List
+ * 				a.ArrayList
+ * 				b.LinkedList
+ * 				c.Vector(已淘汰)
+ * 			BB.Set
+ * 				a.HashSet
+ * 					aa:LinkedHashSet
+ * 				b.TreeSet
+ * 		B、Map
+ * 			双列结合（添加数据的时候，每次添加一对数据）
+ *
+ *
+ * List系列集合：添加的元素是有序、可重复、有索引
+ * 		有序：存的时候和取的时候顺序一致。
+ * 			 如，存的时候为“张三”、“李四”、“王五”，则取的时候也为“张三”、“李四”、“王五”
+ * Set系列集合：添加的元素是无序、不重复、无索引
+ *
+ *
+ * Collection：
+ * 		Collection是单列集合的祖宗接口，它的功能是全部单列集合都可以继承使用的。
+ *
+ *
+ *
+ *
+ *
+ * 		LinedHashMap
+ * 		Map
+ * 		Collection
+ * 		TreeMap
+ * 		HashMap
+ * 		List
+ *
+ * Collection集合
+ *
+ * List集合
+ *
+ * ArrayList集合
+ *
+ * LinkedList集合
+ */
+public class Test {
+}
