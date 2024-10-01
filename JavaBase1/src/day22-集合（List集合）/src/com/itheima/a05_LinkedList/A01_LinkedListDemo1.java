6590e84b3e8029a0632294050a45bf5f2b73f413
daykalif <1038200595@qq.com>
2024-10-02 05:22:39 +0800

    p191 集合进阶-07-LinkedList源码分析

---
+++ JavaBase1/src/day22-集合（List集合）/src/com/itheima/a05_LinkedList/A01_LinkedListDemo1.java
@@ -0,0 +1,21 @@
+package com.itheima.a05_LinkedList;
+
+/*
+ *	LinkedList集合：
+ * 		底层数据结构是双链表，查询慢，增删快，但是如果操作的是首尾元素，速度也是极快的。
+ * 		LinkedList本身多了很多直接操作首尾元素的特有API。
+ *
+ *
+ * 【不常用】：
+ * 	public void addFirst(E e)				在该列表开头插入指定的元素
+ * 	public void addLast(E e)				将指定的元素追加到此列表的末尾
+ * 	public E getFirst()						返回此列表中的第一个元素
+ * 	public E getLast()						返回此列表中的最后一个元素
+ * 	public E removeFirst()					从此列表中删除并返回第一个元素
+ * 	public E removeLast()					从此列表中删除并返回最后一个元素
+ */
+public class A01_LinkedListDemo1 {
+	public static void main(String[] args) {
+
+	}
+}
