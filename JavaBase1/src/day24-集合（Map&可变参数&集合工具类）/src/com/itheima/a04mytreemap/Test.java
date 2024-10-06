83adb21e83db0fb5936cd401dad0081e45ad57e1
daykalif <1038200595@qq.com>
2024-10-06 14:43:42 +0800

    p13 集合进阶-12-TreeMap练习3-利用TreeMap进行统计

--- JavaBase1/src/day24-集合（Map&可变参数&集合工具类）/src/com/itheima/a04mytreemap/Test.java
+++ JavaBase1/src/day24-集合（Map&可变参数&集合工具类）/src/com/itheima/a04mytreemap/Test.java
@@ -10,6 +10,14 @@ package com.itheima.a04mytreemap;
  * 代码书写两种排序规则：
  * 	- 实现Comparable接口，指定比较规则。
  * 	- 创建集合时传递Comparator比较器对象，指定比较规则。
+ *
+ * 总结：
+ * 	1.TreeMap集合的特点是怎么样的？
+ * 		- 不重复、无索引、可排序
+ * 		- 底层基于红黑树实现排序，增删改查性能较好
+ * 	2.TreeMap几个排序的两种方式
+ * 		- 实现Comparable接口，指定比较规则
+ * 		- 创建集合时传递Comparator比较器对象，指定比较规则
  */
 public class Test {
 }
