653c1fe9b8af26fa6e70ee60af651f36fdb52c0e
daykalif <1038200595@qq.com>
2024-10-06 14:23:24 +0800

    p11 集合进阶-10-TreeMap的基本使用和基础练习1

---
+++ JavaBase1/src/day24-集合（Map&可变参数&集合工具类）/src/com/itheima/a04mytreemap/Test.java
@@ -0,0 +1,15 @@
+package com.itheima.a04mytreemap;
+
+/*
+ * TreeMap:
+ * 	- TreeMap跟TreeSet底层原理一样，都是红黑树结构的。
+ * 	- 由键决定特性：不重复、无索引、可排序
+ * 	- 可排序：对键进行排序。
+ * 	- 注意：默认按照键的从小到大进行排序，也可以自己规定键的排序规则
+ *
+ * 代码书写两种排序规则：
+ * 	- 实现Comparable接口，指定比较规则。
+ * 	- 创建集合时传递Comparator比较器对象，指定比较规则。
+ */
+public class Test {
+}
