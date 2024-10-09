fb4406b762f3028a0415e9faa04cf594213aaa63
daykalif <1038200595@qq.com>
2024-10-09 16:54:52 +0800

    p22 集合进阶-21-综合练习1-随机点名器的两种实现方式

---
+++ JavaBase1/src/day24-集合（Map&可变参数&集合工具类）/src/com/itheima/a08call_the_roll/Test1.java
@@ -0,0 +1,28 @@
+package com.itheima.a08call_the_roll;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+/*
+ * 班级里有N个学生，学生属性:姓名，年龄，性别。
+ * 实现随机点名器。
+ */
+public class Test1 {
+	public static void main(String[] args) {
+		// 1.定义集合
+		ArrayList<String> list = new ArrayList<>();
+		// 2.添加数据
+		Collections.addAll(list, "范闲", "范建", "范统", "杜子腾", "杜琦燕", "宋合泛", "侯笼藤", "朱益群", "朱穆朗玛峰", "袁明媛");
+		// 3.随机点名
+		Random r = new Random();
+		int index = r.nextInt(list.size());
+		String name = list.get(index);
+		System.out.println(name);
+
+		// 打乱
+		Collections.shuffle(list);
+		String name1 = list.get(0);
+		System.out.println(name1);
+	}
+}
