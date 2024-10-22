c0e0275fe0b9d126726db6dbceb7f06bd1027e20
daykalif <1038200595@qq.com>
2024-10-22 21:31:17 +0800

    p46 方法引用-04-引用本类或父类的成员方法

---
+++ JavaBase1/src/day26-集合（Stream流&方法引用）/src/com/itheima/a04myfunction/FunctionDemo4/App.java
@@ -0,0 +1,7 @@
+package com.itheima.a04myfunction.FunctionDemo4;
+
+public class App {
+    public static void main(String[] args) {
+        new LoginJFrame();
+    }
+}
---
+++ JavaBase1/src/day26-集合（Stream流&方法引用）/src/com/itheima/a04myfunction/FunctionDemo4/MyJFrame.java
@@ -0,0 +1,10 @@
+package com.itheima.a04myfunction.FunctionDemo4;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+public class MyJFrame extends JFrame {
+	public void method1(ActionEvent e) {
+		System.out.println("引用其他类的成员方法===go===>按钮被点击了");
+	}
+}
