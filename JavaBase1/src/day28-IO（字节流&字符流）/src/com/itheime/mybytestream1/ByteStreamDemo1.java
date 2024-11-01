1c5bd1b53297d5c25b2bbe948a1221f04d82dc77
daykalif <1038200595@qq.com>
2024-11-01 14:22:02 +0800

    p75 IO流-02-IO流的体系和字节输出流基本用法

---
+++ JavaBase1/src/day28-IO（字节流&字符流）/src/com/itheime/mybytestream1/ByteStreamDemo1.java
@@ -0,0 +1,33 @@
+package com.itheime.mybytestream1;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/*
+ * 演示：字节输出流FileOutputStream
+ * 实现需求：写出一段文字到本地文件中。（暂时不写中文）
+ *
+ * 实现步骤：
+ *       创建对象
+ *       写出数据
+ *       释放资源
+ */
+public class ByteStreamDemo1 {
+	public static void main(String[] args) throws IOException {
+		/*
+		 * 1.创建对象
+		 *
+		 * 写出，使用的是输出流：OutputStream
+		 * 子类需要操作本地文件：File
+		 * 因此结合，需要使用的是：FileOutputStream
+		 */
+		String parentPath = "/Users/daykalif/Desktop/Java/JavaBase1/src/day28-IO（字节流&字符流）/src/com/itheime/mybytestream1/a.txt";
+		FileOutputStream fos = new FileOutputStream(parentPath);
+
+		// 2.写出数据
+		fos.write(97);
+
+		// 3.释放资源
+		fos.close();
+	}
+}
---
+++ JavaBase1/src/day28-IO（字节流&字符流）/src/com/itheime/mybytestream1/Test.java
@@ -0,0 +1,13 @@
+package com.itheime.mybytestream1;
+
+/*
+ * FileOutputStream:
+ * 		操作本地文件的字节输出流，可以把程序中的数据写到本地文件中。
+ *
+ * 	书写步骤：
+ * 		1.创建字节流输出流对象
+ * 		2.写数据
+ * 		3.释放资源
+ */
+public class Test {
+}
