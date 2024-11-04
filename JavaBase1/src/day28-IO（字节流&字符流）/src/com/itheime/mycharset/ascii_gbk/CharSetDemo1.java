cd7795b8dd182f47e3124ce5369bd0ed5dada96b
daykalif <1038200595@qq.com>
2024-11-04 16:46:37 +0800

    p86 IO流-13-字符集详解（ASCII，GBK）

---
+++ JavaBase1/src/day28-IO（字节流&字符流）/src/com/itheime/mycharset/ascii_gbk/CharSetDemo1.java
@@ -0,0 +1,21 @@
+package com.itheime.mycharset.ascii_gbk;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/*
+ * 字节流读取中文会出现乱码
+ */
+public class CharSetDemo1 {
+	public static void main(String[] args) throws IOException {
+		String inputPath = "/Users/daykalif/Desktop/Java/JavaBase1/src/day28-IO（字节流&字符流）/src/com/itheime/mycharset/a.txt";
+		FileInputStream fis = new FileInputStream(inputPath);
+
+		int b;
+		while ((b = fis.read()) != -1) {
+			System.out.print((char) b);
+		}
+
+		fis.close();
+	}
+}
