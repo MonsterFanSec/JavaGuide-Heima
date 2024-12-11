fa6eb5ba359d470a030295baae259ff4ed1f7523
daykalif <1038200595@qq.com>
2024-12-11 20:03:59 +0800

    p166 多线程&JUC-32-线程池多大合适

--- JavaBase1/src/day32-多线程&juc/src/com/itheima/mythreadpool/a02threadpool2/MyThreadPoolDemo2.java
+++ JavaBase1/src/day32-多线程&juc/src/com/itheima/mythreadpool/a02threadpool2/MyThreadPoolDemo2.java
@@ -1,5 +1,18 @@
 package com.itheima.mythreadpool.a02threadpool2;
 
+/*
+ * 线程池多大合适呢？
+ *
+ * CPU密集型运算：最大并行数 + 1
+ * I/O密集型运算：最大并行数 * 期望CPU利用率 * （总时间（CPU计算时间+等待时间）/ CPU计算时间）
+ *
+ * 4核8线程：
+ * 	举例：从本地文件中，读取两个数据，并进行相加
+ * 		操作一：读取两个数据（1秒钟）
+ * 		操作二：相加（1秒钟）
+ *
+ * 		8 * 100% * （100% / 50%）= 16
+ */
 public class MyThreadPoolDemo2 {
 	public static void main(String[] args) {
 		// 向Java虚拟机返回可用处理器的数目
