405476d94ced4ea9efe3177c30d4bd8b5056944a
daykalif <1038200595@qq.com>
2024-12-13 21:01:34 +0800

    p174 网络编程-07-协议

---
+++ JavaBase1/src/day33-网络编程/src/com/itheima/a01InetAddressdemo/TCP_IP.java
@@ -0,0 +1,18 @@
+package com.itheima.a01InetAddressdemo;
+
+/*
+ * 协议：
+ * 		计算机网络中，连接和通信的规则被成为网络通信协议
+ *
+ * 	UDP协议：
+ * 		- 用户数据报协议（User Datagram Protocol）
+ * 		- UDP是面向无连接通信协议。
+ * 		- 速度快，有大小限制，一次最多发送64K，数据不安全，易丢失数据
+ *
+ * 	TCP协议：
+ * 		- 传输控制协议TCP（Transmission Control Protocol）
+ * 		- TCP协议是面向连接的通信协议。
+ * 		- 速度慢，没有大小限制，数据安全。
+ */
+public class TCP_IP {
+}
