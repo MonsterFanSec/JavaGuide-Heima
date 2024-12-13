b2ee1848186fd56cc1007c1f0c58a33296195e71
daykalif <1038200595@qq.com>
2024-12-14 07:44:23 +0800

    p179 网络编程-12-TCP协议（发送和接收数据）

---
+++ JavaBase1/src/day33-网络编程/src/a05tcpdemo1/Server.java
@@ -0,0 +1,30 @@
+package a05tcpdemo1;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/*
+ * TCP协议，接收数据
+ */
+public class Server {
+	public static void main(String[] args) throws IOException {
+		// 1.创建对象ServerSocker
+		ServerSocket ss = new ServerSocket(10086);
+
+		// 2.监听客户端的链接
+		Socket socket = ss.accept();
+
+		// 3.从连接通道中获取输入流读取数据
+		InputStream is = socket.getInputStream();
+		int b;
+		while ((b = is.read()) != -1) {
+			System.out.println((char) b);
+		}
+
+		// 4.释放资源
+		socket.close();
+		ss.close();
+	}
+}
