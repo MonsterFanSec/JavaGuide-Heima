77da3c9f559f885a5ad03a657213608cde8a2386
daykalif <1038200595@qq.com>
2024-12-26 21:25:44 +0800

    day36-09-01-xml的介绍

---
+++ JavaBase1/src/day36-基础加强（日志，类加载器，单元测试，xml，注解，羊了个羊）/src/com/itheima/xml/Test1.java
@@ -0,0 +1,49 @@
+package com.itheima.xml;
+
+/*
+ * 什么是配置文件？
+ * 		用来保存程序在运行时需要的一些参数。
+ *
+ * 常见的配置文件：
+ * 		a.txt
+ * 		b.properties
+ * 		c.xml
+ *
+ * 三种配置文件的优缺点：
+ * 		TXT文件：（光看值不知道这个值的作用）
+ * 			C:\idea\background.png
+ * 			微软雅黑
+ * 			23
+ * 			Windows 10 Light
+ *
+ * 		Properties文件：(键值对形式，不适合配置复杂信息)
+ * 			background:day36-08-07-类加载器-常用方法
+ * 			fontType=微软雅黑
+ * 			fontsize=23
+ * 			theme=Windows 10 Light
+ *
+ * 			用户=zhangsan，lisi，wangwu
+ * 			年龄=23，24，25
+ * 			性别=male，female，male
+ *
+ * TXT文件：
+ * 		优点：没有优点。
+ * 		缺点：不利于阅读。
+ *
+ * XML文件：
+ * 		优点：易于阅读。
+ * 			 可以配置出成组出现的数据。
+ * 		缺点：解析比较复杂
+ *
+ * properties文件：
+ * 		优点：键值对形式易于阅读。
+ * 			 解析简单。
+ * 		缺点：无法配置一组一组的数据。
+ *
+ * 以后配置文件怎么选择？
+ * 		数据量较少，一个键只对应一个值，使用properties
+ *
+ *
+ */
+public class Test1 {
+}
