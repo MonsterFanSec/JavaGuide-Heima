0209a043312f3f4937806019f60a6abb58ee0a41
daykalif <1038200595@qq.com>
2024-12-26 21:35:33 +0800

    day36-09-02-xml作为配置文件优势

---
+++ JavaBase1/src/day36-基础加强（日志，类加载器，单元测试，xml，注解，羊了个羊）/src/com/itheima/xml/Test2.java
@@ -0,0 +1,14 @@
+package com.itheima.xml;
+
+/*
+ * XML概述：
+ * 	- XML的全称为（Extensible Markup Language），是一种可扩展的标记语言。
+ * 	- 标记语言：通过标签来描述数据的一门语言（标签有时我们也将其称之为元素）。
+ * 	- 可扩展：标签的名字是可以自己定义的。
+ *
+ * XML作用：
+ * 	- 用于进行存储数据和传输数据
+ * 	- 作为软件的配置文件
+ */
+public class Test2 {
+}
