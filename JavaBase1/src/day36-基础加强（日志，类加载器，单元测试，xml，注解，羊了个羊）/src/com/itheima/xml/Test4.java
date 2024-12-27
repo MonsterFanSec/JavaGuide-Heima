f4b336f74bb6e551c647b37b44c88087795f94c4
daykalif <1038200595@qq.com>
2024-12-27 17:35:22 +0800

    day36-09-06-schema

--- JavaBase1/src/day36-基础加强（日志，类加载器，单元测试，xml，注解，羊了个羊）/src/com/itheima/xml/Test4.java
+++ JavaBase1/src/day36-基础加强（日志，类加载器，单元测试，xml，注解，羊了个羊）/src/com/itheima/xml/Test4.java
@@ -1,6 +1,6 @@
 package com.itheima.xml;
 
-/**
+/*
  * 什么是文档约束？
  * 	- 文档约束：是用来限定xml文件中的标签以及属性应该怎么写。
  * 			  以此强制约束程序员必须按照文档约束的规定来编写xml文件。
@@ -8,6 +8,12 @@ package com.itheima.xml;
  * 文档约束的分类：
  * 	- DTD
  * 	- schema
+ *
+ * XML的文档约束-DTD的作用和问题？
+ *  - 可以约束XML文件的编写。
+ * 	- 不能约束具体的数据类型。
+ *
+ *
  */
 public class Test4 {
 }
