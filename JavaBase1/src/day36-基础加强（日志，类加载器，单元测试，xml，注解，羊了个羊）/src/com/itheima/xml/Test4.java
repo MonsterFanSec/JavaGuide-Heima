91eb09ea6365b64f69b16ef92612f93af03e5ac4
daykalif <1038200595@qq.com>
2024-12-27 17:08:44 +0800

    day36-09-05-dtd约束

---
+++ JavaBase1/src/day36-基础加强（日志，类加载器，单元测试，xml，注解，羊了个羊）/src/com/itheima/xml/Test4.java
@@ -0,0 +1,13 @@
+package com.itheima.xml;
+
+/**
+ * 什么是文档约束？
+ * 	- 文档约束：是用来限定xml文件中的标签以及属性应该怎么写。
+ * 			  以此强制约束程序员必须按照文档约束的规定来编写xml文件。
+ *
+ * 文档约束的分类：
+ * 	- DTD
+ * 	- schema
+ */
+public class Test4 {
+}
